babdeffe178ec6f5820feb9d0a963317cf95b79f
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-20 21:57:01 +0330

    Exploring the Base Types

---
+++ TypeScript-Basic/TypeScript-Intro/basics.ts
@@ -0,0 +1,11 @@
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives: number, string, boolean
+let age: number;
+age = 35;
+
+let userName: string = 'Shahram';
+
+let isTypescript: boolean;
+isTypescript = true;
