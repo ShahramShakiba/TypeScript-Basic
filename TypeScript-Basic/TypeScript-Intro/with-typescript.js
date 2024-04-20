2dd0f92e31efa06dc1b33442f66377a5d5461bf6
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-20 20:59:52 +0330

    Installing & Using TypeScript

---
+++ TypeScript-Basic/TypeScript-Intro/with-typescript.js
@@ -0,0 +1,5 @@
+function addT(a, b) {
+    return a + b;
+}
+var result = addT(2, 5);
+console.log(result);
---
+++ TypeScript-Basic/TypeScript-Intro/with-typescript.ts
@@ -0,0 +1,7 @@
+function addT(a: number, b: number) {
+  return a + b;
+}
+
+const result = addT(2, 5);
+
+console.log(result);
