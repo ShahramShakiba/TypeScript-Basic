02eae108459d7dc4d46b1cf330488127c7fe3d7a
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-21 12:26:13 +0330

    Diving Into Generics Types

--- TypeScript-Basic/TypeScript-Intro/basics.ts
+++ TypeScript-Basic/TypeScript-Intro/basics.ts
@@ -63,5 +63,22 @@ function add3(a: number, b: number): number | string {
 
 function add4(value: any) {
   console.log(value);
-} 
+}
 // this fn doesn't return anything, it has no return statement | it has a special return-type called "void" = is basically comparable with null and undefined but it's only used in conjunction with fns | it means this fn never returns
+
+// 08. generics | we can use generics to create reusable components that work with any type, not just a specific one
+
+// define a generic-type after fn-name <T> t for the type, which will be available inside of this function
+function generic<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const dummyArray = [1, 2, 3];
+
+// with generic-type, typescript is able to understand it should look at the concrete values of the arguments here and it understand that this is an array of numbers or strings, | therefore it's able to infer the type of the array
+const numberArray = generic(dummyArray, -1); // [-1, 1, 2, 3]
+const stringArray = generic(['a', 'b', 'c'], 'd');
+
+numberArray[0].split(''); // we get an error
+stringArray[0].split(''); 
