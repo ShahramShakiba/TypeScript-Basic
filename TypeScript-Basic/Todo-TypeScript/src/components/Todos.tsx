f8ae0e21dfd3d86102c3369cdb2c4e810e54613e
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-21 22:23:25 +0330

    Working with Components & TypeScript

---
+++ TypeScript-Basic/Todo-TypeScript/src/components/Todos.tsx
@@ -0,0 +1,8 @@
+export default function Todos() {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Learn TypeScript</li>
+    </ul>
+  );
+}
--- TypeScript-Basic/Todo-TypeScript/src/index.tsx
+++ TypeScript-Basic/Todo-TypeScript/src/index.tsx
@@ -1,4 +1,3 @@
-import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 
@@ -8,13 +7,4 @@ import './index.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
