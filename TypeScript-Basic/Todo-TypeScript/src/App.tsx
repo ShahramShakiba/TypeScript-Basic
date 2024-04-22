5f2b3942af541f43d2584c8d77ba15be0708aa0a
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-22 14:46:10 +0330

    Working with Props | React.FC<>

--- TypeScript-Basic/Todo-TypeScript/src/App.tsx
+++ TypeScript-Basic/Todo-TypeScript/src/App.tsx
@@ -3,7 +3,7 @@ import Todos from './components/Todos';
 export default function App() {
   return (
     <>
-      <Todos />
+      <Todos items={['learn react', 'learn typescript']} />
     </>
   );
 }
--- TypeScript-Basic/Todo-TypeScript/src/components/Todos.tsx
+++ TypeScript-Basic/Todo-TypeScript/src/components/Todos.tsx
@@ -1,8 +1,22 @@
-export default function Todos() {
+const Todos: React.FC<{ items: string[] }> = ({ items }) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {items.map((item) => (
+        <li key={item}> {item} </li>
+      ))}
     </ul>
   );
-}
+};
+
+export default Todos;
+
+/* Type Annotations - React.FC
+React.FC is a type that stands for "Function Component" in React. It is a generic type that allows you to specify the props that a function component will accept.
+
+? Using React.FC can be helpful for a number of reasons:
+01. It provides type safety for the props that a component expects to receive. This can help catch bugs early on and make your code easier to understand.
+
+02. It makes it easier to refactor your component's props. For example, if you want to rename a prop, the TypeScript compiler will catch any places where that prop is used and help you update them.
+
+03. It makes it easier to see the expected shape of a component's props just by looking at its type definition.
+*/
